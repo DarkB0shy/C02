939255272b34abbfe320d5bf88eb824be3777086
Davide Carassiti <dcarassi@e3r1p5.42roma.it>
2022-10-21 12:36:29 +0200

    Cartella ex00

---
+++ ex00/ft_strcpy.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcarassi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/21 11:41:45 by dcarassi          #+#    #+#             */
+/*   Updated: 2022/10/21 12:17:12 by dcarassi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return(dest);
+}
+
+int	main(void)
+{
+	char	c[5] = "Henlo";
+	ft_strcpy(c, "Jerome");
+	return(0);
+}
