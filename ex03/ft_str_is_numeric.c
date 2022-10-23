dcdb0206db96226a936e2ff53c86198451f6d864
Davide Carassiti <dcarassi@e3r7p3.42roma.it>
2022-10-23 21:51:25 +0200

    Cartella ex03

---
+++ ex03/ft_str_is_numeric.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_numeric.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcarassi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/23 21:28:26 by dcarassi          #+#    #+#             */
+/*   Updated: 2022/10/23 21:50:58 by dcarassi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_str_is_numeric(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[i] == '\0')
+		return (1);
+	while(str[i])
+	{
+		if (str[i] >= 48 && str[i] <= 57)
+			i++;
+		else
+			return (0);
+	}
+	return (1);
+}
+
+int	main(void)
+{
+	char c[] = "";
+	int z = ft_str_is_numeric(c) + '0';
+	write(1, &z, 1);
+	write(1, "\n", 2);
+}
