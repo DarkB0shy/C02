5173851268cb13e8f3538a70e2e2a304366c83c9
Davide Carassiti <dcarassi@e3r4p4.42roma.it>
2022-10-27 09:55:39 +0200

    scizzerodue

--- ex09/ft_strcapitalize.c
+++ ex09/ft_strcapitalize.c
@@ -6,35 +6,50 @@
 /*   By: dcarassi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/23 22:55:46 by dcarassi          #+#    #+#             */
-/*   Updated: 2022/10/26 21:40:01 by dcarassi         ###   ########.fr       */
+/*   Updated: 2022/10/27 09:55:21 by dcarassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 
-char	*ft_strcapitalize(char *str)
+char	*ft_strlowercase(char *str)
 {
 	int	i;
-	int	space;
 
-	space = 0;
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if (str[i] >= 48 && str[i] <= 57)
-			space = 0;
-		else
-			space = 1;
-		if (str[i] >= 97 && str[i] <= 122)
-			if (space == 1)
-				str[i] -= 32;
+		if (str[i] >= 65 && str[i] <= 90)
+			str[i] += 32;
 		i++;
 	}
 	return (str);
 }
-int	main (void)
+
+char	*ft_strcapitalize(char *str)
 {
-	ft_strcapitalize("salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un");
-	return (0);
+	int	i;
+	int	sheck;
+
+	i = 0;
+	sheck = 1;
+	ft_strlowercase(str);
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			if (sheck == 1)
+			{
+				str[i] -= 32;
+				sheck = 0;
+			}
+		}
+		else if (str[i] >= 48 && str[i] <= 57)
+			sheck = 0;
+		else
+			sheck = 1;
+		i++;
+	}
+	return (str);
 }
